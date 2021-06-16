d28806c195fa383df0a5062a27169ab82a4d42df
Lasse Porsch <lasseporsch@gmail.com>
2021-06-16 21:09:58 +0200

    Initial Commit

---
+++ lib/xcpretty_azure_formatter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "xcpretty_azure_formatter/version"
+
+module XcprettyAzureFormatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
